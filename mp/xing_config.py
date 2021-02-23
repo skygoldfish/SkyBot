e71414e40bffc8dd38a9d24f74e5c4461ea5844e
skygoldfish <43559735+skygoldfish@users.noreply.github.com>
2021-02-23 16:10:07 +0900

    소스정리

--- mp/xing_config.py
+++ mp/xing_config.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-RES_FOLDER_PATH = "C:/eBEST/xingAPI/Res"  # xing_tick_crawler Res 파일 폴더 위치
+#RES_FOLDER_PATH = "C:/eBEST/xingAPI/Res"  # xing_tick_crawler Res 파일 폴더 위치
+RES_FOLDER_PATH = ".\Res"  # xing_tick_crawler Res 파일 폴더 위치
 TICKER_DATA_FOLDER_PATH = "."  # tick 데이터 저장할 위치
 
 dt = datetime.now()
