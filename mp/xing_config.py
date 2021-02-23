bba8555f829a8c38bb7481736e41261868b693a4
skygoldfish <43559735+skygoldfish@users.noreply.github.com>
2021-02-23 17:58:50 +0900

    소스정리

--- mp/xing_config.py
+++ mp/xing_config.py
@@ -1,13 +1,20 @@
 from datetime import datetime
+from configparser import ConfigParser
+
+# Configuration Parser
+parser = ConfigParser()
+parser.read('.\skybot.ini')
+
+NightTime_PreStart_Hour = parser.getint('Initial Value', 'NightTime Pre-Start Hour')
 
 #RES_FOLDER_PATH = "C:/eBEST/xingAPI/Res"  # xing_tick_crawler Res 파일 폴더 위치
-RES_FOLDER_PATH = ".\Res"  # xing_tick_crawler Res 파일 폴더 위치
+RES_FOLDER_PATH = "./Res"  # xing_tick_crawler Res 파일 폴더 위치
 TICKER_DATA_FOLDER_PATH = "."  # tick 데이터 저장할 위치
 
 dt = datetime.now()
 
 # 오전 6시 ~ 7시는 Break Time
-if 7 <= dt.hour < 16:
+if 7 <= dt.hour < NightTime_PreStart_Hour:
     # 오전 7시 ~ 오후 3시 59분
     DayTime = True
     NightTime = False
